0b27412b8ab8bc3dbccc4785a46a8c71022de4af
Q2016 <k.sohraby@gmail.com>
2022-07-01 17:37:00 -0700

    Create 1240. Tiling a Rectangle with the Fewest Squares.py

---
+++ 1240. Tiling a Rectangle with the Fewest Squares.py
@@ -0,0 +1,21 @@
+Question:
+Given a rectangle of size n x m, return the minimum number of integer-sided squares that tile the rectangle.
+
+Example 1:
+Input: n = 2, m = 3
+Output: 3
+Explanation: 3 squares are necessary to cover the rectangle.
+2 (squares of 1x1)
+1 (square of 2x2)
+
+
+
+
+
+
+
+
+
+
+
+Solution:
